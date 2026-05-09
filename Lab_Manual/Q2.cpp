7a7e4af0e4cbc11589604bd2c4a6b35f48198a8e
abhinav101010 <71586575+abhinav101010@users.noreply.github.com>
2026-05-09 01:16:30 +0000

    Latest

--- Lab_Manual/Q2.cpp
+++ Lab_Manual/Q2.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n,temp, total = 0;
-    cout << "Enter a positive integer: ";
-    cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        cin >> temp; 
-        if (temp<0)
-        {
-            continue;
-        }
-        
-        total += temp;
-    }
-    cout << "Sum = " << total;;
+    int radius;
+    const float pi = 3.14;
+    cin >> radius;
+    float area = pi * radius * radius;
+    cout << "Area: " << area << endl;
     return 0;
 }
